3fcc9ca493ae52de740e21d868c1def4e9397ec2
Barış DEMİRCİ <hi@338.rocks>
2023-07-24 18:28:00 +0300

    👽️ Update banner API URL

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { env } from "./env";
 export const config = {
 	repoURL: "https://gitea.338.rocks/barbarbar338/threads-daily-random-quote",
 	randomQuoteAPI: "https://api.quotable.io/quotes",
-	bannerAPI: `https://banner.338.rocks/banner?text=%{text}&extension=jpeg`,
+	bannerAPI: `https://api.338.rocks/canvas/banner?message=%{text}&extension=jpeg`,
 	...env,
 	birthdays,
 	avatarURL:
